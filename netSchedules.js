8f1b8e113f992b8e3903acd2b7989750137f1ac7
unknown <Administrator@Lo2.wrldc.local>
2016-11-29 17:42:33 +0530

    obtained peak offpeak mw data

--- netSchedules.js
+++ netSchedules.js
@@ -1,10 +1,10 @@
-function getStateTotalNetMUData(stateStr, day, rev, done) {
+function getStateTotalNetMUData(stateStr, day, rev, header, done) {
     getNetScheduleData(day, rev, function (data) {
         // Search for MWHR row index in second column
         var muRow = -1;
         var muArray = [];
         for (var i = 0; i < data.length; i++) {
-            if (data[i][1] == "MWHR") {
+            if (data[i][1] == header) {
                 muRow = i;
                 break;
             }
@@ -18,11 +18,12 @@ function getStateTotalNetMUData(stateStr, day, rev, done) {
         for (var i = 0; i < stateStr.length; i++) {
             var headingIndex = headings.indexOf(stateStr[i]);
             if (headingIndex == -1) {
-                muArray[i] = {"mu": -1, "Error": "Invalid State Search String", name: stateStr[i]};
+                muArray[i] = {"value": -1, "tag": header, "Error": "Invalid State Search String", name: stateStr[i]};
             } else {
-                muArray[i] = {"mu": (data[muRow][headingIndex]) * 0.001, name: stateStr[i]};
+                muArray[i] = {"value": (data[muRow][headingIndex]) , "tag": header, name: stateStr[i]};
             }
         }
+		//now get the MTOA, STOA, LTA, IEX, PXI MUs
         done({"muArray": muArray});
     });
 }
@@ -34,7 +35,7 @@ function getStateTotalNetMU() {
     getRevisionData(new Date(day), function (data) {
         WriteLineConsole("Latest revision is " + data[0]["Revision"]);
         var rev = data[0]["Revision"];
-        getStateTotalNetMUData([stateStr], new Date(day), rev, function (data) {
+        getStateTotalNetMUData([stateStr], new Date(day), rev, "MWHR", function (data) {
             WriteLineConsole(JSON.stringify(data));
         });
     });
@@ -45,14 +46,49 @@ function getAllStatesTotalNetMU() {
     var stateSelectEl = document.getElementById("stateSelect");
     var stateStrArray = [];
     for (var i = 0; i < stateSelectEl.options.length; i++) {
-        stateStrArray.push(stateSelectEl.options[i].value);
+        //stateStrArray.push(stateSelectEl.options[i].value);
+        stateStrArray.push(stateSelectEl.options[i].innerHTML + " Total");
+        stateStrArray.push(stateSelectEl.options[i].innerHTML + " MTOA");
+        stateStrArray.push(stateSelectEl.options[i].innerHTML + " STOA");
+        stateStrArray.push(stateSelectEl.options[i].innerHTML + " LTA");
+        stateStrArray.push(stateSelectEl.options[i].innerHTML + " IEX");
+        stateStrArray.push(stateSelectEl.options[i].innerHTML + " PXI");
     }
     //get latest revision
     getRevisionData(new Date(day), function (data) {
         WriteLineConsole("Latest revision is " + data[0]["Revision"]);
         var rev = data[0]["Revision"];
-        getStateTotalNetMUData(stateStrArray, new Date(day), rev, function (data) {
-            WriteLineConsole(JSON.stringify(data));
+        getStateTotalNetMUData(stateStrArray, new Date(day), rev, "MWHR", function (data) {
+            //WriteLineConsole(JSON.stringify(data));
+			for (var i = 0; i < data.muArray.length; i++) {
+				WriteLineConsole(JSON.stringify(data.muArray[i]));
+			}
+        });
+    });
+}
+
+function getAllStatesValuesAtHrs(hr) {
+    var day = document.getElementById("dateInp").value;
+    var stateSelectEl = document.getElementById("stateSelect");
+    var stateStrArray = [];
+    for (var i = 0; i < stateSelectEl.options.length; i++) {
+        //stateStrArray.push(stateSelectEl.options[i].value);
+        stateStrArray.push(stateSelectEl.options[i].innerHTML + " Total");
+        stateStrArray.push(stateSelectEl.options[i].innerHTML + " MTOA");
+        stateStrArray.push(stateSelectEl.options[i].innerHTML + " STOA");
+        stateStrArray.push(stateSelectEl.options[i].innerHTML + " LTA");
+        stateStrArray.push(stateSelectEl.options[i].innerHTML + " IEX");
+        stateStrArray.push(stateSelectEl.options[i].innerHTML + " PXI");
+    }
+    //get latest revision
+    getRevisionData(new Date(day), function (data) {
+        WriteLineConsole("Latest revision is " + data[0]["Revision"]);
+        var rev = data[0]["Revision"];
+        getStateTotalNetMUData(stateStrArray, new Date(day), rev, hr, function (data) {
+            //WriteLineConsole(JSON.stringify(data));
+			for (var i = 0; i < data.muArray.length; i++) {
+				WriteLineConsole(JSON.stringify(data.muArray[i]));
+			}
         });
     });
 }
